6ee61c0dfa44cfdde3ccc1d7fb0f53bdcf4b5371
imranalwi01 <imranalwi01@gmail.com>
2026-02-14 15:43:50 +0800

    chore : remove deprecated backend

--- packages/backend/src/api/routes/simple-test.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-
-const router = Router();
-
-router.get('/hello', (req, res) => {
-  res.json({ message: 'Hello from simple test route' });
-});
-
-export default router;
