e2ea79edb8997bb51610b666e962abba25399643
Antonius Joshua <antoniusjoshua47@gmail.com>
2026-05-12 13:58:57 +0700

    feat: update

---
+++ packages/frontend/src/app/banking/ifrs9/page.tsx
@@ -0,0 +1,5 @@
+import { redirect } from 'next/navigation';
+
+export default function Ifrs9IndexPage() {
+  redirect('/banking/ifrs9/impairment-module');
+}
---
+++ packages/frontend/src/app/banking/workflow/staging/page.tsx
@@ -0,0 +1,5 @@
+import { redirect } from 'next/navigation';
+
+export default function WorkflowStagingPage() {
+  redirect('/banking/ifrs9/staging');
+}
